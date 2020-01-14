930d7cb3e32b89579e038a74e710cbe84fc4669a
PHBMdeveloper <phbm@hotmail.com>
2020-01-14 02:35:02 -0300

    fix

--- api/models.py
+++ api/models.py
@@ -5,50 +5,50 @@ from django_mysql.models import ListCharField
 
 # Create your models here.
 class ProximoLancamento(models.Model):
-    nome = models.CharField(max_length=500)
+    nome = models.CharField(max_length=150)
 
     def __str__(self):
         return self.nome
 
 
 class UltimoLancamento(models.Model):
-    nome = models.CharField(max_length=500)
+    nome = models.CharField(max_length=150)
 
     def __str__(self):
         return self.nome
 
 
 class ProximosLancamento(models.Model):
-    flight_number = models.CharField(max_length=500)
-    mission_name = models.CharField(max_length=500)
-    #mission_id = models.CharField(max_length=500) não conseguir importar
-    launch_year = models.CharField(max_length=500)
-    launch_date_unix = models.CharField(max_length=500)
-    launch_date_utc = models.CharField(max_length=500)
-    launch_date_local = models.CharField(max_length=500)
-    is_tentative = models.CharField(max_length=500)
-    tentative_max_precision = models.CharField(max_length=500)
-    tbd = models.CharField(max_length=500)
-    #launch_window = models.CharField(max_length=500)
-    details = models.CharField(max_length=500)
+    flight_number = models.CharField(max_length=150)
+    mission_name = models.CharField(max_length=150)
+    #mission_id = models.CharField(max_length=150) não conseguir importar
+    launch_year = models.CharField(max_length=150)
+    launch_date_unix = models.CharField(max_length=150)
+    launch_date_utc = models.CharField(max_length=150)
+    launch_date_local = models.CharField(max_length=150)
+    is_tentative = models.CharField(max_length=150)
+    tentative_max_precision = models.CharField(max_length=150)
+    tbd = models.CharField(max_length=150)
+    #launch_window = models.CharField(max_length=150)
+    details = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.flight_number
 
 
 class LancamentosPassado(models.Model):
-    flight_number = models.CharField(max_length=500)
-    mission_name = models.CharField(max_length=500)
-    #mission_id = models.CharField(max_length=500) não conseguir importar
-    launch_year = models.CharField(max_length=500)
-    launch_date_unix = models.CharField(max_length=500)
-    launch_date_utc = models.CharField(max_length=500)
-    launch_date_local = models.CharField(max_length=500)
-    is_tentative = models.CharField(max_length=500)
-    tentative_max_precision = models.CharField(max_length=500)
-    tbd = models.CharField(max_length=500)
-    #launch_window = models.CharField(max_length=500)
-    details = models.CharField(max_length=500)
+    flight_number = models.CharField(max_length=150)
+    mission_name = models.CharField(max_length=150)
+    #mission_id = models.CharField(max_length=150) não conseguir importar
+    launch_year = models.CharField(max_length=150)
+    launch_date_unix = models.CharField(max_length=150)
+    launch_date_utc = models.CharField(max_length=150)
+    launch_date_local = models.CharField(max_length=150)
+    is_tentative = models.CharField(max_length=150)
+    tentative_max_precision = models.CharField(max_length=150)
+    tbd = models.CharField(max_length=150)
+    #launch_window = models.CharField(max_length=150)
+    details = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.flight_number
