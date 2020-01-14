a5b0aa6569e075ae7bfe0dc9c91ee3dd8775ee25
PHBMdeveloper <phbm@hotmail.com>
2020-01-14 02:46:31 -0300

    fix

--- api/models.py
+++ api/models.py
@@ -1,54 +1,54 @@
 from django.db import models
 
-from django.db.models import CharField, Model
+from django.db.models import CharField, Model, TextField
 from django_mysql.models import ListCharField
 
 # Create your models here.
 class ProximoLancamento(models.Model):
-    nome = models.CharField(max_length=150)
+    nome = models.TextField()
 
     def __str__(self):
         return self.nome
 
 
 class UltimoLancamento(models.Model):
-    nome = models.CharField(max_length=150)
+    nome = models.TextField
 
     def __str__(self):
         return self.nome
 
 
 class ProximosLancamento(models.Model):
-    flight_number = models.CharField(max_length=150)
-    mission_name = models.CharField(max_length=150)
-    #mission_id = models.CharField(max_length=150) não conseguir importar
-    launch_year = models.CharField(max_length=150)
-    launch_date_unix = models.CharField(max_length=150)
-    launch_date_utc = models.CharField(max_length=150)
-    launch_date_local = models.CharField(max_length=150)
-    is_tentative = models.CharField(max_length=150)
-    tentative_max_precision = models.CharField(max_length=150)
-    tbd = models.CharField(max_length=150)
-    #launch_window = models.CharField(max_length=150)
-    details = models.CharField(max_length=1000)
+    flight_number = models.TextField()
+    mission_name = models.TextField()
+    #mission_id = models.TextField() não conseguir importar
+    launch_year = models.TextField()
+    launch_date_unix = models.TextField()
+    launch_date_utc = models.TextField()
+    launch_date_local = models.TextField()
+    is_tentative = models.TextField()
+    tentative_max_precision = models.TextField()
+    tbd = models.TextField()
+    #launch_window = models.TextField()
+    details = models.TextField()
 
     def __str__(self):
         return self.flight_number
 
 
 class LancamentosPassado(models.Model):
-    flight_number = models.CharField(max_length=150)
-    mission_name = models.CharField(max_length=150)
-    #mission_id = models.CharField(max_length=150) não conseguir importar
-    launch_year = models.CharField(max_length=150)
-    launch_date_unix = models.CharField(max_length=150)
-    launch_date_utc = models.CharField(max_length=150)
-    launch_date_local = models.CharField(max_length=150)
-    is_tentative = models.CharField(max_length=150)
-    tentative_max_precision = models.CharField(max_length=150)
-    tbd = models.CharField(max_length=150)
-    #launch_window = models.CharField(max_length=150)
-    details = models.CharField(max_length=1000)
+    flight_number = models.TextField()
+    mission_name = models.TextField()
+    #mission_id = models.TextField() não conseguir importar
+    launch_year = models.TextField()
+    launch_date_unix = models.TextField()
+    launch_date_utc = models.TextField()
+    launch_date_local = models.TextField()
+    is_tentative = models.TextField()
+    tentative_max_precision = models.TextField()
+    tbd = models.TextField()
+    #launch_window = models.TextField()
+    details = models.TextField()
 
     def __str__(self):
         return self.flight_number
