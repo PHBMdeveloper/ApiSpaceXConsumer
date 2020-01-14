9570fe9b2486fae92588020dbef0c29368988c43
PHBMdeveloper <phbm@hotmail.com>
2020-01-14 05:46:54 -0300

    alguns testes

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from .models import ProximosLancamento, LancamentosPassado
+from rest_framework.response import Response
+import urllib3
+import json
 
-# Create your tests here.
+
+class ProximosLancamentoTestCase(TestCase):
+    def setUp(self):
+        self.ProximosLancamento = ProximosLancamento.objects.create(
+            flight_number="flight_number")
+        self.ProximosLancamento = ProximosLancamento.objects.create(
+            mission_name="mission_name")
+
+    def test_Criacao(self):
+        ''' Teste de criação no db '''
+        assert ProximosLancamento.objects.all().count() == 2
+
+    def test_Excluir(self):
+        ''' Teste de exclusão no db '''
+        ProximosLancamento.objects.all().delete()
+        assert ProximosLancamento.objects.all().count() == 0
+
+    def test_ProximoLancamento(self):
+        http = urllib3.PoolManager()
+        r = http.request('GET', 'https://api.spacexdata.com/v3/launches/next')
+        j = json.loads(r.data.decode('utf-8'))
+        print('ProximoLancamento =', len(j))
+        assert len(j) == 30
+        return Response(j)
+
+    def test_UltimoLancamento(self):
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET', 'https://api.spacexdata.com/v3/launches/latest')
+        j = json.loads(r.data.decode('utf-8'))
+        print('UltimoLancamento =', len(j))
+        assert len(j) == 30
+        return Response(j)
+
+    def test_ProximosLancamentos(self):
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET', 'https://api.spacexdata.com/v3/launches/upcoming')
+        j = json.loads(r.data.decode('utf-8'))
+        print('ProximosLancamentos =', len(j))
+        assert len(j) == 16
+        return Response(j)
+
+    def test_LancamentosPassado(self):
+        http = urllib3.PoolManager()
+        r = http.request(
+            'GET', 'https://api.spacexdata.com/v3/launches/past')
+        j = json.loads(r.data.decode('utf-8'))
+        print('LancamentosPassados =', len(j))
+        assert len(j) == 87
+        return Response(j)
